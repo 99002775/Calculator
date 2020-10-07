2a9e7cdd3c9351e5a7b86ddf25eb753da82452b2
99002775 <72464285+99002775@users.noreply.github.com>
2020-10-07 23:28:52 +0530

    Update main.c

--- sourcecode/main.c
+++ sourcecode/main.c
@@ -1,11 +1,8 @@
 int main() {
     char operator;
-   // double first, second;
+  
     printf("Enter an operator +, -, *,/,%,! ");
     scanf("%c", &operator);
-   // printf("Enter two operands: ");
-   // scanf("%lf %lf", &first, &second);
-
     switch (operator) {
     case '+':
 	    addition();
